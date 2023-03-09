f123bdca7cb254238f92ca98ac6f2bca230b6e72
gabmotamelo <gabrielmota6@hotmail.com>
2023-03-09 10:40:04 -0300

    Criando projeto

---
+++ empresa-api-sonda-it/src/test/java/sonda/it/empresaapi/EmpresaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sonda.it.empresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
